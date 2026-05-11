ed3512e0b640e08f1c202388b404f0c7de3799fa
Dhruv Kumar Singh <dhruvkumarsingh327@gmail.com>
2026-05-11 23:37:06 +0530

    plugins: Fix e2e test failures

--- plugins/pluginctl/src/multi-plugin-management.test.js
+++ plugins/pluginctl/src/multi-plugin-management.test.js
@@ -26,19 +26,19 @@ describe('MultiPluginManagement', () => {
   let installer;
   const PLUGIN_DATA = [
     {
-      name: 'appcatalog_headlamp_plugin',
-      source: 'https://artifacthub.io/packages/headlamp/test-123/appcatalog_headlamp_plugin',
-      version: '0.0.3',
+      name: 'headlamp_flux',
+      source: 'https://artifacthub.io/packages/headlamp/headlamp-plugins/headlamp_flux',
+      version: '0.4.0',
     },
     {
-      name: 'ai_plugin',
-      source: 'https://artifacthub.io/packages/headlamp/test-123/ai_plugin',
-      version: '0.0.2',
+      name: 'headlamp_minikube',
+      source: 'https://artifacthub.io/packages/headlamp/headlamp-plugins/headlamp_minikube',
+      version: '0.3.0',
     },
     {
-      name: 'prometheus_headlamp_plugin',
-      source: 'https://artifacthub.io/packages/headlamp/test-123/prometheus_headlamp_plugin',
-      version: '0.0.3',
+      name: 'headlamp_cert-manager',
+      source: 'https://artifacthub.io/packages/headlamp/headlamp-plugins/headlamp_cert-manager',
+      version: '0.1.0',
     },
   ];
   beforeEach(async () => {
